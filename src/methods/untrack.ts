da13b0c7b554a3fedfa6392993608b8912a76462
apollo79 <bummtschack@mail.de>
2023-06-12 18:35:24 +0200

    refactor(untrack): use CURRENTOBSERVER instead of CURRENTOWNER

--- src/methods/untrack.ts
+++ src/methods/untrack.ts
@@ -1,11 +1,9 @@
-import { CURRENTOWNER, ERRORTHROWN_SYMBOL } from "~/context.ts";
-import { Observer } from "~/objects/observer.ts";
+import { CURRENTOBSERVER, ERRORTHROWN_SYMBOL } from "~/context.ts";
 import { Owner } from "~/objects/owner.ts";
 
 export function untrack<T>(fn: () => T): T {
-  // Only computations are tracking, so if the current scope is a computation we have to explicitly run the callback with no tracking
-  if (CURRENTOWNER instanceof Observer) {
-    const result = Owner.runWithOwner(fn, CURRENTOWNER, undefined);
+  if (CURRENTOBSERVER) {
+    const result = Owner.runWithOwner(fn, CURRENTOBSERVER, undefined);
 
     return result === ERRORTHROWN_SYMBOL ? undefined! : result;
   }
